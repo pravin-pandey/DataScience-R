f52e0deaddbd6918cbfcdb5f1b23cef6681ffede
pravin-pandey <pravin.pandey.dev@gmail.com>
2020-08-11 20:56:39 +0530

    Add files via upload

---
+++ 3. Classification/2. k-Nearest Neighbors(kNN)/kNN.R
@@ -0,0 +1,77 @@
+# Logistic Regression
+
+getwd()
+
+setwd('C://Users//pravi//Desktop//PROJECTS//Data Science - R//3. Classification//2. K-Nearest Neighbours (KNN)')
+
+# Importing Data
+
+df = read.csv('Social_Network_Ads.csv')
+
+# Splitting dataset into train and test set
+
+library(caTools)
+set.seed(123)
+split = sample.split(df$Purchased, SplitRatio=0.75)
+training_set = subset(df,split==TRUE)
+test_set = subset(df,split==FALSE)
+
+#Feature Scaling
+
+training_set[,1:2]= scale(training_set[,1:2])
+test_set[,1:2] = scale(test_set[,1:2])
+
+# Fitting kNN on training set result and Predicting test set result
+install.packages('class')
+library(class)
+y_pred = knn(train = training_set[,-3],
+             test = test_set[,-3],
+             cl = training_set[,3],
+             k = 5)
+
+y_pred
+
+# Making confusion matrix
+
+cm = table(test_set[,3], y_pred)
+
+cm
+
+# Visualising the training set
+
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = knn(train = training_set[,-3],
+             test = grid_set,
+             cl = training_set[,3],
+             k = 5)
+plot(set[, -3],
+     main = 'kNN (Training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+# Visualising the Test set results
+library(ElemStatLearn)
+set = test_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = knn(train = training_set[,-3],
+             test = grid_set,
+             cl = training_set[,3],
+             k = 5)
+plot(set[, -3],
+     main = 'kNN (Test set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
